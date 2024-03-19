12ba065d815099dc8fb51370242eeadb17437c8f
tube-jeonghoon <55288856+tube-jeonghoon@users.noreply.github.com>
2024-03-19 23:33:36 +0900

    Title: 길이 단위 변환하기, Time: 144ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240319/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        double FEETTOCM = 30.48;
+        int MILETOCM = 160934;
+
+        System.out.printf("9.2ft = %.1fcm\n", 9.2*FEETTOCM);
+        System.out.printf("1.3mi = %.1fcm", 1.3*MILETOCM);
+    }
+}
